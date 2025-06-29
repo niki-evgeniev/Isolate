2bf066c4b0b651882e2beca5857b8cb5cde5a608
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-06-29 22:35:27 +0300

    Add spring security

---
+++ src/main/java/shop/AppConfiguration/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package shop.AppConfiguration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private final String rememberMeKey;
+
+    public SecurityConfiguration(@Value("${isolate.remember.me.key}")
+                                 String rememberMeKey) {
+        this.rememberMeKey = rememberMeKey;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                authorizeRequest -> authorizeRequest
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/fonts/**", "/images/**", "/js/**", "/php/**",
+                                "/plugins/**", "/vendor/**", "/css/**", "/sign_in").permitAll()
+                        .requestMatchers("/", "/contact").permitAll()
+                        .anyRequest().authenticated()
+
+        ).formLogin(
+                formLogin -> {
+                    formLogin
+                            .loginPage("/sign_in")
+                            .usernameParameter("email")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/dashboard", true)
+                            .failureForwardUrl("/login-error");
+                }
+        ).logout(
+                logout -> {
+                    logout.logoutUrl("/users/logout")
+                            .logoutSuccessUrl("/")
+                            .invalidateHttpSession(true);
+
+                }
+        ).rememberMe(
+                rememberMe -> {
+                    rememberMe
+                            .key(rememberMeKey)
+                            .rememberMeParameter("remember-me")
+                            .rememberMeCookieName("remember-me");
+                }
+        );
+        return httpSecurity.build();
+    }
+}
