c90e5547fb33b9a6d1dd13b781b948f88e4a635e
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-07 00:53:28 +0300

    Update logjn controller affter resolve CSRF tokens issue

--- src/main/java/shop/Controller/LoginController.java
+++ src/main/java/shop/Controller/LoginController.java
@@ -2,26 +2,30 @@ package shop.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import shop.DTO.UserDto.RegisterUserDTO;
 
 
 @Controller
 public class LoginController {
 
 
-    @GetMapping("/sign_in")
+    @GetMapping("/user/sign_in")
     public ModelAndView login() {
         System.out.println("login controller");
         boolean register = true;
-        ModelAndView modelAndView = new ModelAndView("sign_in");
-        modelAndView.addObject("registerTab", register);
+        ModelAndView modelAndView = new ModelAndView("sign_in OLD");
+//        modelAndView.addObject("registerTab", register);
         return modelAndView;
     }
 
-    @RequestMapping("/login-error")
+
+    @RequestMapping("/users/login-error")
     public ModelAndView errorLogin() {
-        ModelAndView modelAndView = new ModelAndView("sign_in");
+        ModelAndView modelAndView = new ModelAndView("sign_in OLD");
         modelAndView.addObject("bad_credentials", true);
         System.out.println("ERROR LOGIN");
         return modelAndView;
@@ -33,5 +37,10 @@ public class LoginController {
 //        modelAndView.addObject("bad_credentials", true);
 //        return modelAndView;
 //    }
+
+    @ModelAttribute
+    RegisterUserDTO registerUserDTO() {
+        return new RegisterUserDTO();
+    }
 
 }
