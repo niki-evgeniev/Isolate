3ea65c6083bc75d1c485d725fef670d501bd6a3f
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-07 00:52:43 +0300

    update Spring security  resolves an issue with CSRF tokens

--- src/main/java/shop/AppConfiguration/SecurityConfiguration.java
+++ src/main/java/shop/AppConfiguration/SecurityConfiguration.java
@@ -10,8 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import shop.Repository.UserRepository;
 import shop.Service.Impl.ShopUserService;
+
 
 @Configuration
 @EnableMethodSecurity
@@ -19,13 +21,21 @@ public class SecurityConfiguration {
 
     private final String rememberMeKey;
 
+
     public SecurityConfiguration(@Value("${isolate.remember.me.key}")
                                  String rememberMeKey) {
         this.rememberMeKey = rememberMeKey;
+
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        CookieCsrfTokenRepository cookieCsrfTokenRepository  = CookieCsrfTokenRepository.withHttpOnlyFalse();
+//        cookieCsrfTokenRepository.setSecure(true);
+//        cookieCsrfTokenRepository.setCookieCustomizer(cookie -> cookie
+//                        .sameSite("Lax")        // или "Strict", ако не ползваш cross-site форми
+//                        .secure(true));
 
         httpSecurity.authorizeHttpRequests(
                 authorizeRequest -> authorizeRequest
@@ -33,33 +43,27 @@ public class SecurityConfiguration {
                         .requestMatchers("/css/**", "/fonts/**", "/images/**", "/js/**", "/php/**",
                                 "/plugins/**", "/vendor/**").permitAll()
                         .requestMatchers("/robots.txt").permitAll()
-                        .requestMatchers("/", "/contact","/sign_in", "/store",
-                                "/api/***", "/user/signUp", "/register").permitAll()
+                        .requestMatchers("/", "/contact", "/user/sign_in", "/store",
+                                "/api/***", "/user/sign_up", "/register",
+                                "/users/login-error", "/users/logout").permitAll()
                         .anyRequest().authenticated()
-
-//        ).csrf(csrf -> csrf
-//                .requireCsrfProtectionMatcher(request -> {
-//                    String path = request.getServletPath();
-//                    return !path.startsWith("/api/");
-//                })
+        ).csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         ).formLogin(
                 formLogin -> {
                     formLogin
-                            .loginPage("/sign_in")
-//                            .loginProcessingUrl("/sign_in")
+                            .loginPage("/user/sign_in")
                             .usernameParameter("email")
                             .passwordParameter("password")
                             .defaultSuccessUrl("/", true)
-                            .failureForwardUrl("/login-error");
-//                            .failureUrl("/sign_in?error=true");
+                            .failureForwardUrl("/users/login-error");
                 }
-        ).logout(
-                logout -> {
-                    logout.logoutUrl("/users/logout")
-                            .logoutSuccessUrl("/")
-                            .invalidateHttpSession(true);
-
-                }
+        ).logout(logout -> logout
+                .logoutUrl("/users/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
         ).rememberMe(
                 rememberMe -> {
                     rememberMe
@@ -67,7 +71,26 @@ public class SecurityConfiguration {
                             .rememberMeParameter("remember-me")
                             .rememberMeCookieName("remember-me");
                 }
+
+        ).portMapper(
+                httpSecurityHTTPS -> {
+                    httpSecurityHTTPS
+                            .http(80).mapsTo(443)
+                            .http(8080).mapsTo(443);
+                }
+        )
+                .headers(headers -> headers
+                .contentSecurityPolicy(csp -> csp
+                        .policyDirectives(
+                                "default-src 'self'; " +
+                                        "script-src 'self' 'unsafe-inline'; " +
+                                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+                                        "font-src 'self' https://fonts.gstatic.com data:; " +
+                                        "img-src 'self' data:;"
+                        )
+                )
         );
+
         return httpSecurity.build();
     }
 
@@ -75,6 +98,7 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
+
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
