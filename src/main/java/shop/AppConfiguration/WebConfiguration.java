5fc7fd66d3d9d0819e08f994ac828bfad3673019
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-13 02:05:47 +0300

    add ResponseCommitCheckInterceptor

--- src/main/java/shop/AppConfiguration/WebConfiguration.java
+++ src/main/java/shop/AppConfiguration/WebConfiguration.java
@@ -4,18 +4,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import shop.Interceptor.ResponseCommitCheckInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final LocaleChangeInterceptor localeChangeInterceptor;
+    private final ResponseCommitCheckInterceptor responseCommitCheckInterceptor;
 
-    public WebConfiguration(LocaleChangeInterceptor localeChangeInterceptor) {
+    public WebConfiguration(LocaleChangeInterceptor localeChangeInterceptor,
+                            ResponseCommitCheckInterceptor responseCommitCheckInterceptor) {
         this.localeChangeInterceptor = localeChangeInterceptor;
+        this.responseCommitCheckInterceptor = responseCommitCheckInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor);
+        registry.addInterceptor(responseCommitCheckInterceptor);
     }
 }
