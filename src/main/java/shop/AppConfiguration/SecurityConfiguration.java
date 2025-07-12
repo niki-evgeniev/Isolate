9a5e4f93f580c96486c5020fc4f2854bd1e9a311
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-12 17:12:18 +0300

    Update Security conf
    Try to fix error Cannot create a session after the response has been committed

--- src/main/java/shop/AppConfiguration/SecurityConfiguration.java
+++ src/main/java/shop/AppConfiguration/SecurityConfiguration.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import shop.Repository.UserRepository;
 import shop.Service.Impl.ShopUserService;
 
@@ -21,38 +24,33 @@ public class SecurityConfiguration {
 
     private final String rememberMeKey;
 
-
     public SecurityConfiguration(@Value("${isolate.remember.me.key}")
                                  String rememberMeKey) {
         this.rememberMeKey = rememberMeKey;
-
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//
-//        CookieCsrfTokenRepository cookieCsrfTokenRepository  = CookieCsrfTokenRepository.withHttpOnlyFalse();
-//        cookieCsrfTokenRepository.setSecure(true);
-//        cookieCsrfTokenRepository.setCookieCustomizer(cookie -> cookie
-//                        .sameSite("Lax")        // или "Strict", ако не ползваш cross-site форми
-//                        .secure(true));
 
         httpSecurity.authorizeHttpRequests(
                 authorizeRequest -> authorizeRequest
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/css/**", "/fonts/**", "/images/**", "/js/**", "/php/**",
+                        .requestMatchers("/css/**", "/fonts/**", "/images/**", "/php/**",
                                 "/plugins/**", "/vendor/**").permitAll()
                         .requestMatchers("/robots.txt").permitAll()
                         .requestMatchers("/", "/contact", "/user/sign_in", "/store",
-                                "/api/***", "/user/sign_up", "/register",
+                                "/api/***", "/user/sign_up","/users/login", "/register",
                                 "/users/login-error", "/users/logout").permitAll()
                         .anyRequest().authenticated()
-        ).csrf(csrf -> csrf
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//        ).csrf(csrf -> csrf
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+
         ).formLogin(
                 formLogin -> {
                     formLogin
                             .loginPage("/user/sign_in")
+                            .loginProcessingUrl("/users/login")
                             .usernameParameter("email")
                             .passwordParameter("password")
                             .defaultSuccessUrl("/", true)
@@ -71,25 +69,8 @@ public class SecurityConfiguration {
                             .rememberMeParameter("remember-me")
                             .rememberMeCookieName("remember-me");
                 }
-
-        ).portMapper(
-                httpSecurityHTTPS -> {
-                    httpSecurityHTTPS
-                            .http(80).mapsTo(443)
-                            .http(8080).mapsTo(443);
-                }
-        )
-                .headers(headers -> headers
-                .contentSecurityPolicy(csp -> csp
-                        .policyDirectives(
-                                "default-src 'self'; " +
-                                        "script-src 'self' 'unsafe-inline'; " +
-                                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-                                        "font-src 'self' https://fonts.gstatic.com data:; " +
-                                        "img-src 'self' data:;"
-                        )
-                )
         );
+
 
         return httpSecurity.build();
     }
@@ -98,7 +79,6 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
-
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
