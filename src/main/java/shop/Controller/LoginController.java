f36cb98795c227abc054f81d7d9f757eb2ab9f4c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-13 02:06:33 +0300

    Refactoring Login Controller

--- src/main/java/shop/Controller/LoginController.java
+++ src/main/java/shop/Controller/LoginController.java
@@ -1,10 +1,10 @@
 package shop.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import shop.DTO.UserDto.RegisterUserDTO;
 
@@ -12,31 +12,23 @@ import shop.DTO.UserDto.RegisterUserDTO;
 @Controller
 public class LoginController {
 
-
     @GetMapping("/user/sign_in")
-    public ModelAndView login() {
+    public ModelAndView login(HttpServletRequest request) {
+        request.getSession(true);
         System.out.println("login controller");
-        boolean register = true;
-        ModelAndView modelAndView = new ModelAndView("sign_in OLD");
-//        modelAndView.addObject("registerTab", register);
+        ModelAndView modelAndView = new ModelAndView("login");
         return modelAndView;
     }
 
 
     @RequestMapping("/users/login-error")
-    public ModelAndView errorLogin() {
-        ModelAndView modelAndView = new ModelAndView("sign_in OLD");
+    public ModelAndView errorLogin(HttpServletRequest request) {
+        request.getSession(true);
+        ModelAndView modelAndView = new ModelAndView("login");
         modelAndView.addObject("bad_credentials", true);
         System.out.println("ERROR LOGIN");
         return modelAndView;
     }
-
-//    @GetMapping("/login-error")
-//    public ModelAndView errorLoginGet() {
-//        ModelAndView modelAndView = new ModelAndView("sign_in");
-//        modelAndView.addObject("bad_credentials", true);
-//        return modelAndView;
-//    }
 
     @ModelAttribute
     RegisterUserDTO registerUserDTO() {
