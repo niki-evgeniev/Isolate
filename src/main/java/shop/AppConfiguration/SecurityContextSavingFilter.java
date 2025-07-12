57bd5410ac1bc23fba5d0f54b21fbf6adf638594
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-12 17:11:20 +0300

    Add SecurityContextSavingFilter
    Try to fix error Cannot create a session after the response has been committed

---
+++ src/main/java/shop/AppConfiguration/SecurityContextSavingFilter.java
@@ -0,0 +1,28 @@
+package shop.AppConfiguration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityContextSavingFilter  extends OncePerRequestFilter {
+
+    private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+        SecurityContext context = SecurityContextHolder.getContext();
+        securityContextRepository.saveContext(context, request, response);
+
+    }
+}
