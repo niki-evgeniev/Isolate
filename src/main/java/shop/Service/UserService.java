f47d74d61f1a51845264c1898c005c45e1241640
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-13 02:20:47 +0300

    Update service

--- src/main/java/shop/Service/UserService.java
+++ src/main/java/shop/Service/UserService.java
@@ -1,8 +1,13 @@
 package shop.Service;
 
+import jakarta.validation.Valid;
+import shop.DTO.UserDto.RegisterUserDTO;
+
 public interface UserService {
 
     void addAdminProfile();
 
     boolean checkEmailExist(String email);
+
+    boolean registerNewUser(RegisterUserDTO registerUserDTO);
 }
