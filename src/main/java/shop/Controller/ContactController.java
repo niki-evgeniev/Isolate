05fd1abf9063b36ce938bfd96557c13686a2e0d1
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-13 02:06:10 +0300

    Update Contact Controller

--- src/main/java/shop/Controller/ContactController.java
+++ src/main/java/shop/Controller/ContactController.java
@@ -1,15 +1,31 @@
 package shop.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import shop.DTO.ContactDTO;
 
 @Controller
 public class ContactController {
 
 
     @GetMapping("/contact")
-    public ModelAndView contact(){
+    public ModelAndView contact(HttpServletRequest request){
+        request.getSession(true);
         return new ModelAndView("contact");
+    }
+
+    @PostMapping("/contact")
+    public ModelAndView contact(@Valid ContactDTO contactDTO){
+        return new ModelAndView("redirect:/");
+    }
+
+    @ModelAttribute
+    ContactDTO contactDTO() {
+        return new ContactDTO();
     }
 }
