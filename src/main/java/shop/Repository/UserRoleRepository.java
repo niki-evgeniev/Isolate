3c2e90f2424556398f89406920c8c7a438f4231f
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-13 02:20:20 +0300

    Add find by RoleType

--- src/main/java/shop/Repository/UserRoleRepository.java
+++ src/main/java/shop/Repository/UserRoleRepository.java
@@ -2,8 +2,13 @@ package shop.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import shop.Entity.Enums.RoleType;
 import shop.Entity.UserRole;
+
+import java.util.List;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+     List<UserRole> findByRoleType(RoleType roleType);
 }
