4ae71ca74925436afad44af66308ff083c56a2f4
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-20 03:03:50 +0300

    Upodate repo for scheduled

--- src/main/java/shop/Repository/UserRepository.java
+++ src/main/java/shop/Repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import shop.Entity.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +15,11 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByVerificationToken(String token);
+
+//    List<User> findAllByActivateFalseAndVerificationTokenIsNotNullAndTokenCreatedBefore(LocalDateTime localDateTime);
+
+    List<User> findByActivateFalseAndVerificationTokenIsNotNullAndTokenCreatedBefore(LocalDateTime localDateTime);
+
 }
