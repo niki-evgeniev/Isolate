4415a09ff6a298cb60dea1a50915e23e603df156
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-07-29 03:11:06 +0300

    Add store controller

--- src/main/java/shop/Controller/StoreController.java
+++ src/main/java/shop/Controller/StoreController.java
@@ -1,17 +1,35 @@
 package shop.Controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import shop.DTO.Porduct.ListAllProductDTO;
+import shop.Service.ProductService;
+
 
 @Controller
 public class StoreController {
 
+    private final ProductService productService;
+
+    public StoreController(ProductService productService) {
+        this.productService = productService;
+    }
+
 
     @GetMapping("/store")
-    public ModelAndView store (){
+    public ModelAndView store(@PageableDefault(size = 6, sort = "name") Pageable pageable,
+                              HttpServletRequest request) {
+        Page<ListAllProductDTO> listAllProduct = productService.listAllProduct(pageable);
+        ModelAndView modelAndView = new ModelAndView("storeList");
+        modelAndView.addObject("products", listAllProduct);
+
         System.out.println("Store controller");
-        return new ModelAndView("storeProducts");
+        return modelAndView;
     }
 }
